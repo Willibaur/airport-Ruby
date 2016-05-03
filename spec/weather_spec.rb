fe221b94bb9a41f3fae34d9e913ed8b74290290f
William Bautista <williambautista@gmail.com>
2016-05-03 21:23:15 +0100

    Weather coverage increased up to 100%

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,5 +1,16 @@
 require 'weather'
 
 describe Weather do
-    it { is_expected.to respond_to :stormy? }
+
+  subject(:weather) { described_class.new }
+
+  before do
+    allow_any_instance_of(Kernel).to receive(:rand).and_return(8)
+  end
+
+  describe '#stormy?' do
+    it 'returns stormy weather conditions' do
+      expect(weather.stormy?).to eq true
+    end
+  end
 end
